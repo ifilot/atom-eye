621fe6eae78a9f69954b4f8a617f1351fadd4773
ifilot <ivo@ivofilot.nl>
2026-01-17 17:09:29 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    if (!glfwInit())
+        return -1;
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "MinGW + GLFW", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
